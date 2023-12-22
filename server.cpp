0d631a437cfea29274c8554255c87ef43be53cd4
verilye <connor.j.logan@outlook.com>
2023-12-23 00:46:44 +1000

    Built out server a bit

--- server.cpp
+++ server.cpp
@@ -24,6 +24,8 @@
 //
 //connect() is used on the client side to connect to the server
 
+const size_t k_max_msg = 4096;
+
 static void msg(const char *msg){
 	fprintf(stderr, "%s\n", msg);
 }
@@ -47,6 +49,100 @@ static void do_something(int connfd){
 	char wbuf[] = "world";
 	write(connfd, wbuf, strlen(wbuf));
 }
+
+// Reads from the kernel until n bytes are read
+static int32_t read_full(int fd, char *buf, size_t n){
+	
+	while(n>0){
+	
+		ssize_t rv = read(fd,buf,n);
+		if(rv <=0){
+			return -1;
+		}
+
+		assert((size_t)rv <=n);
+		n -= (size_t)rv;
+		buf += rv;
+
+	}
+
+	return 0;
+}
+
+// Writes data back to the kernel
+static int32_t write_all(int fd, const char *buf, size_t n){
+	while(n>0){
+		ssize_t rv = write(fd, buf, n);
+		
+		if(rv<=0){
+			return -1;
+		}
+
+		assert((size_t)rv <= n);
+
+		n-=(size_t)rv;
+
+		buf+=rv;
+
+	}
+
+	return 0;
+}
+
+static int32_t one_request(int connfd){
+	
+	// 4 bytes header
+	char rbuf[4 + k_max_msg + 1];
+	
+	errno = 0;
+	
+	int32_t err = read_full(connfd, rbuf, 4);
+	
+	if(err){
+		if(errno == 0){
+			msg("EOF");
+		}else{
+			msg("read() error");
+		}
+
+		return err;
+	}
+
+	uint32_t len = 0;
+
+	memcpy(&len, rbuf, 4); // assyme little endian
+	
+	if(len > k_max_msg){
+		msg("too long");
+		return -1;
+	}
+
+	// request body
+	err = read_full(connfd, &rbuf[4], len);
+
+	if(err) {
+		msg("read() error");
+		return err;
+	}
+
+	// do something
+	
+	rbuf[4 + len] = '\0';
+	printf("client says: %s\n", &rbuf[4]);
+
+
+	// reply using the same protocol
+	
+	const char reply[] = "world";
+	char wbuf[4 + sizeof(reply)];
+	len = (uint32_t)strlen(reply);
+	memcpy(wbuf, &len, 4);
+	memcpy(&wbuf[4], reply, len);
+	
+	return write_all(connfd, wbuf, 4 + len);
+
+}
+
 
 int main(){
 	// AF_INET is for IPv4, SOCK_STREAM is for TCP
@@ -75,13 +171,26 @@ int main(){
 	while(true){
 		// accept
 		struct sockaddr_in client_addr = {};
+
 		socklen_t socklen = sizeof(client_addr);
+		
 		int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
 		if(connfd <0){
-			continue;
+			continue; // error
 		}
-
+		
+	
 		do_something(connfd);
+
+		//Read through one request using the linux system code tied to connfd
+		while(true){
+			int32_t err = one_request(connfd);
+
+			if(err){
+				break;
+			}
+
+		}
 		close(connfd);
 	}
 
